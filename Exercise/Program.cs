dd81bc794e780b3b0df888c35ca590b4ebf23478
Sofia <kovaleva.sonia.02@mail.ru>
2023-12-03 13:16:30 +0300

    Add start

---
+++ Exercise/Program.cs
@@ -0,0 +1,8 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует 
+// новый массив из строк, длина которых меньше, либо равна 3 символам. 
+
+
+﻿string[] array1 = new string[5] {"1234", "1567", "-2", "computer science"};
+string[] array2 = new string[array1.Length];
+SecondArrayWithIF(array1, array2);
+PrintArray(array2);
