d3b9bda04d0d00c6b240684a4ac8e61f4ae7edbf
Sofia <kovaleva.sonia.02@mail.ru>
2023-12-03 13:18:46 +0300

    Add print array

--- Exercise/Program.cs
+++ Exercise/Program.cs
@@ -13,7 +13,14 @@ void SecondArray(string[] array1, string[] array2)
         }
     }
 }
-
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
 
 ﻿string[] array1 = new string[5] {"1234", "1567", "-2", "computer science"};
 string[] array2 = new string[array1.Length];
